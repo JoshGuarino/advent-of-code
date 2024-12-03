2d3541056c7d2f12ad9b938576c07d0ba2cecfbe
Josh Guarino <jguarino722@gmail.com>
2024-12-03 13:41:42 -0500

    day2 part2 complete

--- day2/main.py
+++ day2/main.py
@@ -6,15 +6,16 @@ def parse_data():
             data.append(report)  
     return data
 
+# check safety under original rules
 def check_safety(report):
     # flags for checking change is direction 
     increasing = False
     decreasing = False
 
     # loop through until second to last iteration
-    for index in range(len(report)-1):
+    for index, num in enumerate(report[:-1]):
         # calculate difference between current and next num
-        diff = report[index] - report[index+1]
+        diff = num - report[index+1]
 
         # out of bounds check
         if diff == 0 or abs(diff) > 3:
@@ -33,13 +34,37 @@ def check_safety(report):
     # if we make it through all iterations its safe
     return True
 
+# check safety if 1 bad level removed
+def check_safety_minus_one(report):
+    # check if report safe without modification 
+    if check_safety(report):
+        return True
+
+    # check if safe when excluding current level
+    for index, _ in enumerate(report):
+        modified_report = report[:index] + report[index + 1:]
+        if check_safety(modified_report):
+            return True
+
+   # if we make it thorugh all iterations its unsafe 
+    return False
+
 def main():
     data = parse_data()   
-    safe_reports = 0
+    safe_reports_p1 = 0
+    safe_reports_p2 = 0
+
     for report in data:
+        # part one
         if check_safety(report):
-            safe_reports += 1
-    print(safe_reports)
+            safe_reports_p1 += 1
+        
+        # part two
+        if check_safety_minus_one(report):
+            safe_reports_p2 += 1
+
+    print(safe_reports_p1)
+    print(safe_reports_p2) 
 
 if __name__ == "__main__":
     main()
